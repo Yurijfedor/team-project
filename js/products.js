53a1276814c7bc601058e7fcd1fbac4fad782e01
Yurij  Shaklak <yurijfedor178@gmail.com>
2022-06-12 08:33:55 +0300

    add

--- js/products.js
+++ js/products.js
@@ -1,25 +1,8 @@
 $(document).ready(function () {
   $(".slider-cream").slick({
     arrows: false,
-    dots: false,
-    adaptiveHeight: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 1000,
-    easing: "ease",
-    infinite: true,
-    initialSlide: 2,
-    autoplay: false,
-    autoplaySpeed: 1500,
-    pauseOnFocus: true,
-    pauseOnHover: true,
-    pauseOnDotsHover: true,
-    draggable: false,
-    centerMode: false,
-    variableWidth: false,
-    rows: 1,
-    slildesPerRow: 1,
-    vertical: false,
+    dots: true,
+    appendDots: $(".dots--cream"),
   });
 
   $(".products-list__button--cream").click(function (event) {
@@ -30,25 +13,8 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".slider-coffee").slick({
     arrows: false,
-    dots: false,
-    adaptiveHeight: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 1000,
-    easing: "ease",
-    infinite: true,
-    initialSlide: 2,
-    autoplay: false,
-    autoplaySpeed: 1500,
-    pauseOnFocus: true,
-    pauseOnHover: true,
-    pauseOnDotsHover: true,
-    draggable: false,
-    centerMode: false,
-    variableWidth: false,
-    rows: 1,
-    slildesPerRow: 1,
-    vertical: false,
+    dots: true,
+    appendDots: $(".dots--coffee"),
   });
 
   $(".products-list__button--coffee").click(function (event) {
@@ -59,25 +25,8 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".slider-milkshakes").slick({
     arrows: false,
-    dots: false,
-    adaptiveHeight: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 1000,
-    easing: "ease",
-    infinite: true,
-    initialSlide: 2,
-    autoplay: false,
-    autoplaySpeed: 1500,
-    pauseOnFocus: true,
-    pauseOnHover: true,
-    pauseOnDotsHover: true,
-    draggable: false,
-    centerMode: false,
-    variableWidth: false,
-    rows: 1,
-    slildesPerRow: 1,
-    vertical: false,
+    dots: true,
+    appendDots: $(".dots--milkshakes"),
   });
 
   $(".products-list__button--milkshakes").click(function (event) {
